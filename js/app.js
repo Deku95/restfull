1184227bac7df9dd3fb3ebae76e2234500562131
Antonio De Cusati <deku95@hotmail.it>
2016-10-20 17:08:46 +0200

    pausa

--- js/app.js
+++ js/app.js
@@ -25,7 +25,6 @@ console.log(titolo);
 		}).then(function(data){
 			var tr="";
 			for (var i=0;i<data.Search.length;i++){
-				
 				var obj=data.Search[i];
 				console.log(obj);
 				tr += "<tr id='"+ obj.imdbID +"'>";
@@ -45,4 +44,88 @@ console.log(titolo);
 	
 	});
 
+	$("tbody").on("click",'tr',function(){	
+			// ($("#vedi").css("display")=="none") ? $("#vedi").show() :  $("#vedi").hide();
+			if ($("#vedi").css("display")=="none"){
+				$("#vedi").show()	
+			}else{
+				$("#vedi").hide()
+			}
+			
+			
+			if ($("#film").css("display")=="none"){
+				$.ajax({
+			url:"http://www.omdbapi.com/?t="+id,
+			method:"GET",
+		}).then(function(data){
+			var tr="";
+			for (var i=0;i<data.Search.length;i++){
+				var obj=data.Search[i];
+				console.log(obj);
+				tr += "<tr>";
+				if (obj.Poster !='N/A'){
+					tr+="<td class='col-md-3'><img id='locandinag' src='"+ obj.Poster  +"'/></td>";
+				}else {
+					tr+="<td class='col-md-3'><img id='locandinag' src='http://placehold.it/200x350'/></td>";
+				}
+				// un td con le informazioni
+				tr+="<td class='col-md-3'> <ul>	<li>			</li><li>			</li><li>			</li><li>			</li></ul></td>";
+				
+				
+				
+				
+				
+				tr+="<td class='col-md-3'>" + obj.Year + " </td>";
+				tr+="<td class='col-md-3'> " + obj.Type + "</td>";
+				tr+="<td class='col-md-0' id='invis'> " + obj.imdbID + "</td>";
+				tr+="</tr>";
+			}
+			$("#listafilm").html(tr);
+		});
+				
+				
+				
+				$("#film").show()
+				console.log($(this).attr("id"));
+			}else{
+				$("#film").hide()
+			}
+			
+			
+			
+			
+			
+			// ($("#film").css("display")=="none") ? $("#film").show() :  $("#film").hide();
+			
+		});
+	$("#film").on("click",function(){			
+			($("#vedi").css("display")=="none") ? $("#vedi").show() :  $("#vedi").hide();
+			($("#film").css("display")=="none") ? $("#film").show() :  $("#film").hide();
+		});		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
