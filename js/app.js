8de252e24517753973315d1a17e02484e1a5a3f9
Antonio De Cusati <deku95@hotmail.it>
2016-10-20 12:45:01 +0200

    provato a fare la table

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
-
+	$("#search").submit(function(event){
+		event.preventDefault();
+		
+		var titolo=$("input[name='titolo']").val();
+		console.log(titolo);
+		$.ajax({
+			url:"http://www.omdbapi.com/?s="+titolo,
+			method:"GET"
+		}).then(function(data){
+			var tr="";
+			for (var i=0;i<data.Search.length;i++){
+				
+				var obj=data.Search[i];
+				tr += "<tr"+obj.Title+">";
+				tr+="<td>" + obj.Years + "</td>";
+				tr+="<td>" + obj.imdbID + "</td>";
+				tr+="<td>" + obj.Type + "</td>";
+				tr+="<td>" + obj.Poster + "</td>";
+				tr+="</tr>";
+					
+			}
+			$("#listafilm").html(tr);
+		});
+	
+	});
 
 });
